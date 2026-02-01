a2e52e11711dde6d68ef7bbc0314d982a4cc4e31
artem <artemtarakanov2505@gmail.com>
2026-02-01 14:29:13 +0300

    Реализована разметка хэдера

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import './Header.css';
+
+export default function Header(){
+    return(
+
+        <header>
+            <h1 className ='logotype'>ЛОР ВРАЧ МСК</h1>
+
+            <ul className ='nav'>
+
+                <li className ='nav-item'>
+                    <a href = '#' className="about-me">О враче</a>
+                </li>
+
+                <li className ='nav-item'>
+                    <a href = '#' className="services">Услуги</a>
+                </li>
+
+                <li className ='nav-item'>
+                    <a href = '#' className="online">Онлайн</a>
+                </li>
+
+                <li className ='nav-item'>
+                    <a href = '#' className="appointment">Приём</a>
+                </li>
+
+                <li className ='nav-item'>
+                    <a href = '#' className="comments">Отзывы</a>
+                </li>
+
+            </ul>
+
+            <button className = 'logo-signup'>Записаться</button>
+
+        </header>
+    )
+}
